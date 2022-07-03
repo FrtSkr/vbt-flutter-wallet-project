2e8a59d2d54e6c6fe4ee4842c6124106153a2fd5
Fırat Şakar <frtskr0@gmail.com>
2022-07-03 23:47:31 +0300

    added icon widgets

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wallet_project/widgets/icon_widget.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -61,10 +62,79 @@ class _HomeState extends State<Home> {
               children: [
                 //Kartlar Listesi
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     //Icon widgets
+                    IconWidget(
+                      title: "Send",
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      delayAnimation: 1.5,
+                      color: const Color(0xFF17334E),
+                    ),
+                    IconWidget(
+                      title: "Pay",
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.payment,
+                          color: Colors.red,
+                        ),
+                      ),
+                      delayAnimation: 1.7,
+                      color: const Color(0xFF411C2E),
+                    ),
+                    IconWidget(
+                      title: "Withdraw",
+                      child: InkWell(
+                        onTap: () {},
+                        child: Image.asset(
+                          "images/icons8-cash-withdrawal-96.png",
+                          width: MediaQuery.of(context).size.height * 0.08,
+                          height: MediaQuery.of(context).size.height * 0.08,
+                          color: Colors.green,
+                        ),
+                      ),
+                      delayAnimation: 1.9,
+                      color: const Color(0xFF163333),
+                    ),
+                    IconWidget(
+                      title: "Bill",
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.receipt,
+                          color: Colors.purple,
+                        ),
+                      ),
+                      delayAnimation: 2.1,
+                      color: const Color(0xFF32204D),
+                    ),
+                    IconWidget(
+                      title: "Voucher",
+                      child: InkWell(
+                        onTap: () {},
+                        child: Image.asset(
+                          "images/icons8-voucher-96.png",
+                          width: MediaQuery.of(context).size.height * 0.08,
+                          height: MediaQuery.of(context).size.height * 0.08,
+                          color: Colors.orange,
+                        ),
+                      ),
+                      delayAnimation: 2.3,
+                      color: const Color(0xFF412D27),
+                    ),
                   ],
-                )
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                //History Wallet
               ],
             ),
           ),
